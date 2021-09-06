249f689e3a920b7c12e1e33629f73edbe7ecf7e5
Srinidhi Mergu <67421239+SrinidhiMergu@users.noreply.github.com>
2021-09-06 22:16:54 +0530

    Create square_root.cpp

---
+++ searching/square_root.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define rep(i, n) for(int i=0;i<n;i++)
+#define repn(i, n) for(int i=1;i<n;i++)
+
+float square_root(int no, int n){
+    //integer part
+    int s=0, e=no;
+    float valid_ans;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(mid*mid==no){
+            return mid;
+        }
+        else if(mid*mid<no){
+            valid_ans=mid;
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    //fractional part
+    float inc=0.1;
+    rep(i,n){
+        while(valid_ans*valid_ans<=no){
+            valid_ans += inc;
+        }
+        valid_ans -= inc;
+        inc/=10;
+    }
+    return valid_ans;
+
+}
+int main(){
+    int n, places;
+    cin>>n>>places;
+    cout<<square_root(n, places)<<endl;
+    return 0;
+}
+
