214937ba7cc0952b4e6e73068d76fda722a36d0c
Srinidhi Mergu <67421239+SrinidhiMergu@users.noreply.github.com>
2021-09-15 06:41:56 +0530

    Update dfs.cpp

--- graphs/dfs.cpp
+++ graphs/dfs.cpp
@@ -41,6 +41,7 @@ void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &storeDfs) {
 vector<int> dfsOfGraph(int V, vector<int> adj[]){
         vector<int> storeDfs; 
         vector<int> vis(V+1, 0); 
+    // runs over all disjoint or disconnected graphs
       for(int i = 1;i<=V;i++) {
         if(!vis[i]){
             dfs(i, vis, adj, storeDfs);
@@ -63,6 +64,7 @@ int main(){
         for(int i = 0; i < E; i++)
         {
             cin >> u >> v;
+            // Undirected graph
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
